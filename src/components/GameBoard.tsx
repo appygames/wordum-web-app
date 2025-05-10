1f93dad736b52b58d52baa3c68f9c035ae436d34
Sonakshi Mathur <sonakshi@appykan.com>
2025-05-11 01:31:06 +0530

    Add tailwind to gameboard

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -86,24 +86,26 @@ export default function GameBoard() {
   });
 
   return (
-    <div className="gameboard-container">
-      <div className="top-bar">
-        <div className="left-icons">
+    <div className="min-w-screen min-h-screen p-0 flex flex-col justify-between items-center bg-[#7DF9FF]">
+      <div className="relative w-full flex items-center justify-between py-2.5 px-5 gap-2.5 text-md">
+        <div className="flex items-center gap-2.5">
           <IoIosArrowBack
-            className="game-icon"
+            className="cursor-pointer"
             size={32}
-            style={{ cursor: "pointer", color: "#000" }}
+            style={{ color: "#000" }}
             onClick={() => router.push("/game")}
           />
           <FaArrowRotateLeft
-            className="game-icon"
+            className="cursor-pointer"
             style={{ cursor: "pointer", color: "#000" }}
             onClick={() => setShowModal(true)}
           />
-          <span className="level-button">{level?.toUpperCase()}</span>
+          <span className="bg-[#004c66] text-white py-1 px-3 font-bold">
+            {level?.toUpperCase()}
+          </span>
         </div>
-        <div className="hints-container">
-          <div className="icons-group">
+        <div className="absolute top-24 md:static left-1/2 md:left-0 md:transform-none md:translate-x-0 transform -translate-x-1/2  w-fit m-auto flex flex-col items-center justify-center gap-2.5">
+          <div className="flex gap-2 text-yellow-300 text-3xl">
             {/*show bulbs based on attempts */}
             {[...Array(3)].map((_, index) => (
               <FaLightbulb
@@ -112,26 +114,30 @@ export default function GameBoard() {
               />
             ))}
           </div>
-          <span className="attempts">{attempts}/3 attempts</span>
+          <span className="md:text-2xl bg-[#004c66] py-1 px-2 rounded-sm font-semibold md:font-bold">
+            {attempts}/3 attempts
+          </span>
         </div>
-        <div className="right-icons">
-          <FaVolumeUp
-            className="game-icon"
-            style={{ cursor: "pointer", color: "#000" }}
-          />
+        <div className="flex items-center gap-3">
+          <FaVolumeUp className="cursor-pointer" style={{ color: "#000" }} />
           <BsBrightnessHighFill
-            className="game-icon"
-            style={{ cursor: "pointer", color: "#000" }}
+            className="cursor-pointer"
+            style={{ color: "#000" }}
           />
-          <div className="coin">{coins}</div>
+          <div className="flex justify-center items-center bg-yellow-300 size-10 rounded-full font-bold">
+            {coins}
+          </div>
         </div>
       </div>
 
-      <div className="gameboard">
+      <div className="w-full">
         {/* Circle Grid */}
-        <div className="circle-grid">
+        <div className="flex flex-col gap-2.5 items-center mb-10">
           {grid.map((row: string[], rowIndex: number) => (
-            <div className="circle-row" key={rowIndex}>
+            <div
+              className="flex py-2 px-6 rounded-lg gap-3 bg-[#01C1C1]"
+              key={rowIndex}
+            >
               {row.map((letter: string, colIndex: number) => {
                 const feedbackColor = feedback[rowIndex]?.[colIndex] as
                   | "green"
@@ -140,7 +146,7 @@ export default function GameBoard() {
                   | undefined;
                 return (
                   <div
-                    className={`circle ${feedbackColor || ""}`}
+                    className={`size-11 md:size-16 bg-cyan-800 rounded-full flex items-center justify-center text-white cursor-pointer text-xl md:text-3xl md:font-semibold bg-${feedbackColor}-400`}
                     key={colIndex}
                     onClick={() => {
                       if (feedbackColor === "green") return;
@@ -161,16 +167,18 @@ export default function GameBoard() {
         </div>
 
         {/* Keyboard */}
-        <div className="keyboard-container">
-          <div className="keyboard">
+        <div className="w-full flex justify-center items-center py-5 gap-3 bg-cyan-700">
+          <div className="flex justify-center items-center flex-wrap max-w-sm gap-3">
             {keyboard.map((char, index) => {
               const shouldDisable = disabledButtons.includes(index);
               return (
                 <button
                   key={index}
-                  className={`key ${shouldDisable ? "used" : ""} ${
-                    index === currentChar && "selected"
-                  }`}
+                  className={`min-h-10 min-w-10 py-1.5 px-1 md:px-3 bg-cyan-900 text-white rounded-sm md:font-bold text-xl md:text-3xl cursor-pointer ${
+                    shouldDisable
+                      ? "bg-gray-400 text-gray-500 cursor-not-allowed"
+                      : ""
+                  } ${index === currentChar && "text-cyan-200"}`}
                   onClick={() => handleKeyClick(char, index)}
                   disabled={shouldDisable}
                 >
