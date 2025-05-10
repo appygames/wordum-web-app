b7cb817ee4f0dc35d09c0078d1d9948e78f24c5e
Sonakshi Mathur <sonakshi@appykan.com>
2025-05-11 00:06:08 +0530

    Add tailwind in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,26 +1,56 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faTwitter, faInstagram, faTelegram, faDiscord } from "@fortawesome/free-brands-svg-icons";
-import "../styles/footer.css"
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+  faTelegram,
+  faDiscord,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
-    <footer className="footer">
-      <div className="footer-links">
-        <div>
-        <a href="#">Privacy Policy</a>
-        </div>
-        <div>
-        <a href="#">Terms and Conditions</a>
-        </div>
+    <footer className="bg-[#004c6b] p-5  fixed bottom-0 w-full hidden md:flex flex-col gap-4 text-left">
+      <div className="flex flex-col">
+        <a href="#" className="font-bold text-xl hover:underline">
+          Privacy Policy
+        </a>
+        <a href="#" className="font-bold text-xl hover:underline">
+          Terms and Conditions
+        </a>
       </div>
-      <div className="social-icons">
-        <a href="#"><FontAwesomeIcon icon={faDiscord} className="icon"/></a>
-        <a href="#"><FontAwesomeIcon icon={faFacebook} className="icon" /></a>
-        <a href="#"><FontAwesomeIcon icon={faTwitter} className="icon" /></a>
-        <a href="#"><FontAwesomeIcon icon={faInstagram} className="icon" /></a>
-        <a href="#"><FontAwesomeIcon icon={faTelegram} className="icon"/></a>
+      <div className="flex gap-2">
+        <a href="#">
+          <FontAwesomeIcon
+            icon={faDiscord}
+            className="size-4 transition-colors ease-in-out duration-300 hover:text-[#00aced]"
+          />
+        </a>
+        <a href="#">
+          <FontAwesomeIcon
+            icon={faFacebook}
+            className="size-4 transition-colors ease-in-out duration-300 hover:text-[#00aced]"
+          />
+        </a>
+        <a href="#">
+          <FontAwesomeIcon
+            icon={faTwitter}
+            className="size-4 transition-colors ease-in-out duration-300 hover:text-[#00aced]"
+          />
+        </a>
+        <a href="#">
+          <FontAwesomeIcon
+            icon={faInstagram}
+            className="size-4 transition-colors ease-in-out duration-300 hover:text-[#00aced]"
+          />
+        </a>
+        <a href="#">
+          <FontAwesomeIcon
+            icon={faTelegram}
+            className="size-4 transition-colors ease-in-out duration-300 hover:text-[#00aced]"
+          />
+        </a>
       </div>
-      <p>Copyright © Wordum</p>
+      <p className="text-sm mt-2">Copyright © Wordum</p>
     </footer>
   );
 }
