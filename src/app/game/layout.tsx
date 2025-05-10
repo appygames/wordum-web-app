df0216deb410fbb4ed1e64c4bb20213d91b752d1
Sonakshi Mathur <sonakshi@appykan.com>
2025-05-11 00:45:23 +0530

    Add tailwind to level selection

--- src/app/game/layout.tsx
+++ src/app/game/layout.tsx
@@ -1,16 +1,7 @@
-// import Header from "@/components/Header";
-// import Footer from "@/components/Footer";
 export default function RootLayout({
   children,
-}: { children: React.ReactNode }) {
-  // const pathname = usePathname(); 
-  // const hideLayout = pathname.startsWith("/game"); 
-
-  return (
-  <>
-       
-        <main className="main-content">{children}</main>
-      
-  </> 
-  );
+}: {
+  children: React.ReactNode;
+}) {
+  return <main>{children}</main>;
 }
--- src/components/LevelSelection.tsx
+++ src/components/LevelSelection.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useRouter } from "next/navigation";
-import "../styles/level-selection.css";
 import { useState } from "react";
 import { FaRegUserCircle } from "react-icons/fa";
 import { IoSettingsOutline } from "react-icons/io5";
@@ -12,9 +11,9 @@ export default function LevelSelection({ onClose }: { onClose: () => void }) {
   const [level, setLevel] = useState("");
 
   return (
-    <div className="popup-overlay">
-      <nav className="mobile-nav">
-        <div className="left-icons">
+    <div className="fixed top-0 left-0 w-full h-full bg-[#80FFFF] flex flex-col items-center justify-center">
+      <nav className="flex items-center justify-between w-full py-3 px-5 md:hidden">
+        <div className="flex gap-2.5">
           <IoIosArrowBack
             size={32}
             style={{ cursor: "pointer", color: "#000" }}
@@ -26,36 +25,46 @@ export default function LevelSelection({ onClose }: { onClose: () => void }) {
           <IoSettingsOutline size={32} color="black" />
         </div>
       </nav>
-      <div className="popup-content">
+      <div className="w-80 m-auto flex flex-col items-center">
         <RxCross1 className="close-icon" onClick={onClose} />
-        <div className="level-selection">
-          <h2 className="popup-title">Choose your preferred level</h2>
+        <div className="w-full flex flex-col items-center gap-4">
+          <h2 className="text-black font-bold text-xl md:text-2xl md:mb-8">
+            Choose your preferred level
+          </h2>
           <button
-            className={level === "easy" ? "selected-level" : ""}
+            className={`h-12 w-full max-w-72 text-xl rounded-lg bg-[#004c6b] cursor-pointer font-bold hover:bg-[#0056b3] active:bg-green-400 translate-0.5 ${
+              level === "easy" && "bg-green-400"
+            }`}
             onClick={() => setLevel("easy")}
           >
             EASY
           </button>
           <button
-            className={level === "medium" ? "selected-level" : ""}
+            className={`h-12 w-full max-w-72 text-xl rounded-lg bg-[#004c6b] cursor-pointer font-bold hover:bg-[#0056b3] translate-0.5 ${
+              level === "medium" && "bg-green-400"
+            }`}
             onClick={() => setLevel("medium")}
           >
             MEDIUM
           </button>
           <button
-            className={level === "hard" ? "selected-level" : ""}
+            className={`h-12 w-full max-w-72 text-xl rounded-lg bg-[#004c6b] cursor-pointer font-bold hover:bg-[#0056b3] translate-0.5 ${
+              level === "hard" && "bg-green-400"
+            }`}
             onClick={() => setLevel("hard")}
           >
             HARD
           </button>
           <button
-            className={level === "expert" ? "selected-level" : ""}
+            className={`h-12 w-full max-w-72 text-xl rounded-lg bg-[#004c6b] cursor-pointer font-bold hover:bg-[#0056b3] translate-0.5 ${
+              level === "expert" && "bg-green-400"
+            }`}
             onClick={() => setLevel("expert")}
           >
             EXPERT
           </button>
           <button
-            className="start-button"
+            className="h-12 w-full max-w-72 text-xl rounded-lg bg-gray-400 hover:bg-gray-600 cursor-pointer font-bold  translate-0.5"
             disabled={!level}
             onClick={() => router.push(`/game/play?level=${level}`)}
           >
