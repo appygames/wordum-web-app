2e1fe4126381a839e85fbaf883d9227b7c443ab4
Sonakshi Mathur <sonakshi@appykan.com>
2025-05-17 15:46:40 +0530

    Fix buttons width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,28 +7,28 @@ export default function Home() {
   return (
    <div className="flex flex-col items-center bg-[#F4C9EC] min-h-screen max-h-screen overflow-hidden">
   <Header />
-  <div className="flex flex-col gap-4 w-xs h-[260px] md:w-1/4 m-auto items-center justify-center text-white mt-60 md:mt-10">
+  <div className="flex flex-col gap-4 h-[260px] md:w-1/4 w-[90%] m-auto items-center justify-center text-white mt-60 md:mt-auto">
     <Link
       href="/game"
-      className="w-[333px] md:w-[90%] text-center py-3 px-5 text-xl rounded-lg bg-[#2258B9] cursor-pointer font-bold hover:bg-[#0056b3] transition-all"
+      className="w-[80%] md:w-[90%] text-center py-3 px-5 text-xl rounded-lg bg-[#2258B9] cursor-pointer font-bold hover:bg-[#0056b3] transition-all"
     >
       PLAY GAME
     </Link>
     <Link
       href="/game/create"
-      className="w-[333px] md:w-[90%] text-center py-3 px-5 text-xl rounded-lg bg-[#2258B9] cursor-pointer font-bold hover:bg-[#0056b3] transition-all"
+      className="w-[80%] md:w-[90%] text-center py-3 px-5 text-xl rounded-lg bg-[#2258B9] cursor-pointer font-bold hover:bg-[#0056b3] transition-all"
     >
       CREATE
     </Link>
     <Link
       href="/game/join"
-      className="w-[333px]  md:w-[90%]  text-center py-3 px-5 text-xl rounded-lg bg-[#2258B9] cursor-pointer font-bold hover:bg-[#0056b3] transition-all"
+      className="w-[80%]  md:w-[90%]  text-center py-3 px-5 text-xl rounded-lg bg-[#2258B9] cursor-pointer font-bold hover:bg-[#0056b3] transition-all"
     >
       ENTER CODE
     </Link>
     <Link
       href="/daily-wordum"
-      className="w-[333px]  md:hidden text-center py-3 px-5 text-xl rounded-lg bg-[#2258B9] cursor-pointer font-bold hover:bg-[#0056b3] transition-all "
+      className="w-[80%]  md:hidden text-center py-3 px-5 text-xl rounded-lg bg-[#2258B9] cursor-pointer font-bold hover:bg-[#0056b3] transition-all "
     >
       DAILY WORDUM
     </Link>
