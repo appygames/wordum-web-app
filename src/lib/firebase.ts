9664c89f8c020b8502b499e2a5225be533637bd2
Sonakshi Mathur <sonakshi@appykan.com>
2025-05-28 22:23:35 +0530

    Add function to get game by id

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyCetmeNwc4J3gDzcoYaARwyP3McEsMR4FI",
   authDomain: "wordum-game-5f739.firebaseapp.com",
@@ -8,10 +8,27 @@ const firebaseConfig = {
   storageBucket: "wordum-game-5f739.firebasestorage.app",
   messagingSenderId: "157228117213",
   appId: "1:157228117213:web:5258dbcff2ccc03187bf34",
-  measurementId: "G-8QXZDYFDZX"
+  measurementId: "G-8QXZDYFDZX",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export { db };
+
+export const getGameById = async (id: string) => {
+  try {
+    const docRef = doc(db, "userGames", id);
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+      return { id: docSnap.id, ...docSnap.data() };
+    } else {
+      console.log("No such document!");
+      return null;
+    }
+  } catch (error) {
+    console.error("Error getting document:", error);
+    return null;
+  }
+};
