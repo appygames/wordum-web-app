e1470c87937f86b207e76b3faa03e626b1a30947
Sonakshi Mathur <sonakshi@appykan.com>
2025-05-28 23:24:47 +0530

    Move credentials to env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCetmeNwc4J3gDzcoYaARwyP3McEsMR4FI",
-  authDomain: "wordum-game-5f739.firebaseapp.com",
-  databaseURL: "https://wordum-game-5f739-default-rtdb.firebaseio.com",
-  projectId: "wordum-game-5f739",
-  storageBucket: "wordum-game-5f739.firebasestorage.app",
-  messagingSenderId: "157228117213",
-  appId: "1:157228117213:web:5258dbcff2ccc03187bf34",
-  measurementId: "G-8QXZDYFDZX",
+   apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
